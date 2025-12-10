aeb4e896418ee29e5ebe58965b6f8ea0b3876a45
liOnux <lio07@mailo.com>
2025-12-10 18:37:35 +0100

    Etape 5 => ok. 70 lignes de code.

--- assets/script.js
+++ assets/script.js
@@ -18,18 +18,17 @@ const slides = [
 		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
 	},
 ]
-const nbImages = slides.length
-const left = document.querySelector("#banner .arrow_left")
-const right = document.querySelector("#banner .arrow_right")
-const pBanner = document.querySelector("#banner p")
-const imgBanner = document.querySelector(".banner-img")
-const parentDot = document.querySelector("div .dots")
+const left = document.querySelector("#banner .arrow_left") // flèche gauche
+const right = document.querySelector("#banner .arrow_right") // flèche droite
+const pBanner = document.querySelector("#banner p") // paragraphe du carrousel
+const imgBanner = document.querySelector(".banner-img") // image du carrousel
+const parentDot = document.querySelector("div .dots") // conteneur parent des dots
 let i = 0 // index du tableau slides[]
 let img_selected = slides[i].image // Image sélectionnée du carrousel
 
 // Fonction qui ajoute les dots :
 function dotsFunction() {
-	for (let i = 0; i < nbImages; i++) {
+	for (let i = 0; i < slides.length; i++) {
 		const dotSpan = document.createElement("span")
 			if (slides[i].image === img_selected) {
 				dotSpan.classList.add("dot_selected")
@@ -41,25 +40,31 @@ function dotsFunction() {
 }
 
 // Fonction qui gère tous les changements après click :
-function imgFunction(i) {
-	imgBanner.src = `./assets/images/slideshow/${slides[i].image}`
+function clickFunction(i) {
+	imgBanner.src = `./assets/images/slideshow/${slides[i].image}` // Change l'image
 	img_selected = slides[i].image
-	pBanner.innerHTML = slides[i].tagLine
+	pBanner.innerHTML = slides[i].tagLine // Change le paragraphe
 	const spans = parentDot.querySelectorAll("span")
 		spans.forEach(span => {
-			parentDot.removeChild(span)
+			parentDot.removeChild(span) // Efface les dots
 	})
-	dotsFunction()
+	dotsFunction() // Remet les dots
 }
 
 dotsFunction()
 
-// Évennements sur les flèches :
+// Évennements après click sur les flèches :
 right.addEventListener("click", () => {
 	i++
-	imgFunction(i)
+	if (i >= slides.length) {
+		i = 0 // Retour à la première image
+	}
+	clickFunction(i)
 })
 left.addEventListener("click", () => {
 	i--
-	imgFunction(i)
+	if (i < 0) {
+		i = slides.length-1 // Retour à la dernière image
+	}
+	clickFunction(i)
 })
